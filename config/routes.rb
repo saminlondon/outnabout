c021e09f7324b80b0a51718331f13a92e542b3a9
Kamal Minott <kamalminott@gmail.com>
2022-11-29 16:24:31 +0000

    nav bar with working buttons and drop down

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :activities
   resources :venues
+  get "my_venues", to: 'venues#my_venues'
   resources :bookings
 
   # Defines the root path route ("/")
