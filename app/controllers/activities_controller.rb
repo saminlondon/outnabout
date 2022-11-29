2ed22b0a90e2d135681ccb496278b6e20fc57b76
Do Yeon Kim <doyeon1105@gmail.com>
2022-11-29 15:27:47 +0000

    venue controller update

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,42 +1,53 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @activities = Activity.all
   end
+
   def new
+    @venue = Venue.find(params[:venue_id])
     @activity = Activity.new
     # @venue = Venue.where(current_user.id == :user_id)
   end
+
+
+
   def show
-    @activity = Activity.find(params[:id])
   end
+
   def create
     @activity = Activity.new(activity_params)
-    @venues = Venue.where(:id == current_user.id)
+    @activity.venue = Venue.where(:venue_id == current_user.id)
     if @activity.save
       redirect_to activity_path(@activity)
     else
       render :new, status: :unprocessable_entity
     end
   end
+
   def edit
-    @activity = Activity.find(params[:id])
   end
+
   def update
-    @activity = Activity.find(params[:id])
     @activity.update(activity_params)
     redirect_to activity_path(@activity)
   end
+
   def destroy
-    @activity = Activity.find(params[:id])
     @activity.destroy!
-    redirect_to activities_path, notice: "Activity was successfully destroyed."
+    redirect_to activities_path
 
   end
 
   private
 
   def activity_params
-    params.require(:activity).permit(:name, :description, :operational_start_time, :operational_end_time, :activity_party_size,
-    :price, :category)
+    params.require(:activity).permit(:name, :description, :operational_start_time, :operational_end_time, :activity_party_size, :price, :category)
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
   end
 end
