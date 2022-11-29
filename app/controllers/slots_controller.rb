03117b597beeeae3a9a8c1a70f2fc0156b97c38a
Do Yeon Kim <doyeon1105@gmail.com>
2022-11-29 14:27:10 +0000

    slots controller made

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,2 +1,40 @@
 class SlotsController < ApplicationController
+  def index
+    @slots = Slot.all
+  end
+
+  def new
+    @slot = Slot.new
+  end
+
+  def show
+    @slot = Slot.find(params[:id])
+  end
+
+  def create
+    @slot = Slot.new(slot_params)
+    @slot.save
+  end
+
+  def edit
+    @slot = Slot.find(params[:id])
+  end
+
+  def update
+    @slot = Slot.find(params[:id])
+    @slot.update(slot_params)
+    redirect_to slot_path(@slot)
+  end
+
+  def destroy
+    @slot = Slot.find(params[:id])
+    @slot.destroy!
+  end
+
+  private
+
+  def slot_params
+    params.require(:slot).permit(:start_time, :end_time, :is_available)
+  end
+
 end
