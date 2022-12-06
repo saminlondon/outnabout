6aab1c511de1986634b421b56931856dd555a351
Do Yeon Kim <doyeon1105@gmail.com>
2022-12-06 15:53:28 +0000

    seed file added

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_101043) do
     t.bigint "activity_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "activity_date"
     t.index ["activity_id"], name: "index_slots_on_activity_id"
   end
 
