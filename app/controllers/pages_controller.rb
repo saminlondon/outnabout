ade5c8412bf4e1fb8e361d3c8ee814558c5dda3d
maaz sattar <maazsattar@gmail.com>
2022-12-06 12:24:32 +0000

    home page revamp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :profile ]
 
   def home
     @activities = Activity.all
     @venues = Venue.all
+  end
+
+  def profile
+    @venues = Venue.where(user: current_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'profile', to: "pages#profile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :activities do
     resources :bookings, except: %i[index destroy]
